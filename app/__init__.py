e641206c48ed82ddf7b8452ec359a51fca13e69a
Illuminater1 <romashcn@gmail.com>
2025-07-21 20:44:03 +0300

    Добавлена панель администратора

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,20 @@ from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_migrate import Migrate
 
+
 from app.db import db
 from app.shop.models import Category, Product
 from app.carts.models import Cart
 from app.orders.models import Order, OrderedProduct
 from app.user.models import User
+from app.admin.views import init_admin
 
 from app.user.forms import LoginForm
 from app.config import SECRET_KEY
+
 from app.user.views import blueprint as user_blueprint
 from app.shop.views import blueprint as shop_blueprint
-from app.admin.views import blueprint as admin_blueprint
+# from app.admin.views import blueprint as admin_blueprint
 from app.main.views import blueprint as main_blueprint
 from app.carts.views import blueprint as cart_blueprint
 from app.orders.views import blueprint as orders_blueprint
@@ -20,28 +23,36 @@ from app.orders.views import blueprint as orders_blueprint
 
 def create_app():
     app = Flask(__name__)
+
     app.config.from_pyfile("config.py")
+
     db.init_app(app)
     migrate = Migrate(app, db)
+
 
     login_manager = LoginManager()
     login_manager.init_app(app)
     login_manager.login_view = 'user.login'
     login_manager.login_message = "Пожалуйста авторизуйтесь"
 
-    app.register_blueprint(user_blueprint)
+
+
     app.register_blueprint(shop_blueprint)
-    app.register_blueprint(admin_blueprint)
+    # app.register_blueprint(admin_blueprint)
     app.register_blueprint(main_blueprint)
     app.register_blueprint(cart_blueprint)
     app.register_blueprint(orders_blueprint)
+    app.register_blueprint(user_blueprint)
+
 
     @login_manager.user_loader
     def load_user(user_id):
-        return User.query.get(user_id)
+        return User.query.get(int(user_id))
 
     @app.errorhandler(404)
     def page_error(error):
         return render_template('page_error.html'), 404
+
+    init_admin(app)
 
     return app
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,14 +1,69 @@
-from flask import Blueprint, render_template
-
-from app.user.decorators import admin_required
-from flask_login import current_user, login_required
-
-blueprint = Blueprint('admin', __name__, url_prefix='/admin')
+from flask import Blueprint, render_template, url_for, redirect
+from flask_admin import Admin, AdminIndexView
+from flask_admin.contrib.sqla import ModelView
 
 
-@blueprint.route('/')
-@admin_required
-def admin_index():
-    title = 'Админ'
-    content = "Страница для админа"
-    return render_template("admin/index.html", page_title=title, content=content)
+from app.user.models import User
+from app.orders.models import Order, OrderedProduct
+from app.shop.models import Product, Category
+from app.db import db
+from flask_login import current_user
+
+# blueprint = Blueprint('admin', __name__, url_prefix='/admin')
+
+
+class AdminView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.role == 'admin'
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('login.login'))
+
+admin = Admin(name='АДМИНКА',
+              template_mode='bootstrap4',
+              index_view=AdminView(url='/admin', endpoint='admin'))
+
+
+class UsersForAdmin(ModelView):
+    column_list = ['username',
+                   'email',
+                   'role']
+
+    form_columns = ['username',
+                    'email',
+                    'role']
+
+class OrdersForAdmin(ModelView):
+    column_list = ['delivery_method',
+                   'status',
+                   'payment',
+                   'user_id']
+
+    form_columns = ['delivery_method',
+                    'status',
+                    'payment',
+                    'user_id']
+
+
+class OrdersForAdmin(ModelView):
+    column_list = ['delivery_method',
+                   'status',
+                   'payment',
+                   'user_id',
+                   'created_time',]
+
+    form_columns = ['delivery_method',
+                    'status',
+                    'payment',
+                    'user_id']
+
+
+def init_admin(app):
+    admin.add_view(UsersForAdmin(User, db.session, name='Пользователи', endpoint='admin_users'))
+    admin.add_view(OrdersForAdmin(Order, db.session, name='Заказы', endpoint='admin_orders'))
+    admin.add_view(ModelView(Category, db.session, name='Категории'))
+    admin.add_view(ModelView(Product, db.session, name='Товары'))
+    admin.add_view(ModelView(OrderedProduct, db.session, name='Заказанные товары'))
+
+    admin.init_app(app)
+
